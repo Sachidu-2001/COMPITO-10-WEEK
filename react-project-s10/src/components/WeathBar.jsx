cbb79c42aaa373d385e5f787c970afd6c9383491
Sachidu-2001 <fsachi0001@gmail.com>
2025-01-17 11:55:42 +0100

    commit

--- react-project-s10/src/components/WeathBar.jsx
+++ react-project-s10/src/components/WeathBar.jsx
@@ -1,15 +1,17 @@
 import { Navbar,Nav,Container } from "react-bootstrap"
+import * as Icon from 'react-bootstrap-icons'
 
 function WeathBar() {
     return(
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className="bg-primary">
       <Container>
-        <Navbar.Brand href="#">The WeatherApp</Navbar.Brand>
+        <Navbar.Brand href="#" className="text-white"><Icon.BrightnessHighFill /><h1>Te WeatherApp</h1></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse>
           <Nav className="me-auto">
             <Nav.Link href="#">Home</Nav.Link>
-            <Nav.Link href="#">Link</Nav.Link>
+            <Nav.Link href="#">Regione</Nav.Link>
+            <Nav.Link href="#">Info</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
