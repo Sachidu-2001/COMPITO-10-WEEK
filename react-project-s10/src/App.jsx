dca35bcce1a1b4b7dcd957eae5f68995732be8ee
Sachidu-2001 <fsachi0001@gmail.com>
2025-01-17 13:02:00 +0100

    export

--- react-project-s10/src/App.jsx
+++ react-project-s10/src/App.jsx
@@ -1,13 +1,15 @@
-//import "./App.css";
+import "./App.css";
 import WeathBar from "./components/WeathBar";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import WeathFooter from "./components/WeathFooter";
+import WeathCards from "./components/WeathCards";
 
 function App() {
   return (
     <>
       <WeathBar />
+      <WeathCards/>
       <WeathFooter/>
     </>
   );
--- react-project-s10/src/components/WeathFooter.jsx
+++ react-project-s10/src/components/WeathFooter.jsx
@@ -1,13 +1,41 @@
 import { Container } from "react-bootstrap";
 
-
-
-
 function WeathFooter() {
-    return(
-        <Container className="bg-primary">
-        </Container>
-    )
+  return (
+    <Container className="bg-primary">
+      <div>
+        <div>
+          <div>Previsioni Regioni</div>
+          <div>
+            <div>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+            </div>
+            <div>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+              <p>Piemonte</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 }
 
-export default WeathFooter
+export default WeathFooter;
