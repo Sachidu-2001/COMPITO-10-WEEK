4298283be8c49c8cfaaa18982ff6b0cc0ef69d56
Sachidu-2001 <fsachi0001@gmail.com>
2025-01-17 15:44:04 +0100

    finished WeathCards

--- react-project-s10/src/App.jsx
+++ react-project-s10/src/App.jsx
@@ -3,11 +3,14 @@ import WeathBar from "./components/WeathBar";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import WeathFooter from "./components/WeathFooter";
+import WeathCards from "./components/WeathCards";
+
 
 function App() {
   return (
     <>
       <WeathBar />
+      <WeathCards/>
       <WeathFooter />
     </>
   );
