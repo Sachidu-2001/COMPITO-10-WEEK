59290b920af3ca42aee2613ce95c0cf9e84dea82
Sachidu-2001 <fsachi0001@gmail.com>
2025-01-17 14:43:21 +0100

    REMOVE

--- react-project-s10/src/App.jsx
+++ react-project-s10/src/App.jsx
@@ -3,14 +3,12 @@ import WeathBar from "./components/WeathBar";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import WeathFooter from "./components/WeathFooter";
-import WeathCards from "./components/WeathCards";
 
 function App() {
   return (
     <>
       <WeathBar />
-      <WeathCards/>
-      <WeathFooter/>
+      <WeathFooter />
     </>
   );
 }
