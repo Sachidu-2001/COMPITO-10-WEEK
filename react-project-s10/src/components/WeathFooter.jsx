0d996d524f4075d888731cb5caa4aacbc883043b
Sachidu-2001 <fsachi0001@gmail.com>
2025-01-17 14:54:32 +0100

    improve footer

--- react-project-s10/src/components/WeathFooter.jsx
+++ react-project-s10/src/components/WeathFooter.jsx
@@ -2,39 +2,44 @@ import { Container } from "react-bootstrap";
 
 function WeathFooter() {
   return (
-    <Container className="bg-primary">
+    <div className="bg-primary">
+    <Container>
       <div>
-        <div>
-          <div>Previsioni Regioni</div>
-          <div>
-            <div>
+        <div className="d-flex justify-content-center">
+          <div className="d-flex">
+            <div className="me-5">
+              <p>Valle D'Aosta</p>
               <p>Piemonte</p>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
+              <p>Liguria</p>
+              <p>Lombardia</p>
+              <p>Veneto</p> 
+              </div>
+              <div className="me-5">
+              <p>Trentino-Alto-Adige</p>
+              <p>Friuli-Venezia-Giulia</p>
+              <p>Toscana</p>
+              <p>Emilia-Romagna</p>
+              <p>Umbria</p>
             </div>
-            <div>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
-              <p>Piemonte</p>
+            <div className="me-5">
+              <p>Marche</p>
+              <p>Abruzzo</p>
+              <p>Lazio</p>
+              <p>Campania</p>
+              <p>Molise</p>
+              </div>
+              <div>
+              <p>Calabria</p>
+              <p>Puglia</p>
+              <p>Basilicata</p>
+              <p>Sicilia</p>
+              <p>Sardegna</p>
             </div>
           </div>
         </div>
       </div>
     </Container>
+    </div>
   );
 }
 
