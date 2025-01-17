1d620f1718999dc0cb236a4105078e5f835bb91d
Sachidu-2001 <fsachi0001@gmail.com>
2025-01-17 14:26:21 +0100

    fetch complete

--- react-project-s10/src/components/WeathCards.jsx
+++ react-project-s10/src/components/WeathCards.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
+import { Container, Row } from "react-bootstrap";
 
 function WeathCards() {
-  const [City, setCity] = useState([]);
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
-    const findWeather = async () => {
+    const findWeather = async (cityname) => {
       try {
         const response = await fetch(
-          `api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
+          `api.openweathermap.org/data/2.5/forecast?q=${cityname}&appid=bbc6ad64da844d903705ded4dc362775`
         );
         const data = await response.json();
         setCity(data.list ? data.list : []);
@@ -16,20 +18,42 @@ function WeathCards() {
       }
     };
 
-    findWeather();
-    
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      findWeather();
+    };
   });
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <Container>
+        <Row>
+            <h2>Trova le Previsioni</h2>
+            <Form></Form>
+        </Row>
+
+
+
+
+
+
+
+
+      <Row>
+        {city.map((element) => (
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title></Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        ))}
+      </Row>
+    </Container>
   );
 }
+
+export default WeathCards;
